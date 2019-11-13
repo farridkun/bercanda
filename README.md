363fe3987e0b2d95cb3c0d65e127c7a651fb8794
farridkun <farridguntoro@gmail.com>
2019-11-13 15:26:47 +0700

    First Bercanda Upload

---
+++ README.md
@@ -0,0 +1,8 @@
+#Bercanda!
+
+This is a some middle project from KunCreative, made by love for
+any business to bring them grow and expand to many target.
+
+bercanda.digital
+instagram.com/bercanda.digital
+hai@bercanda.digital
---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Horaay Bercanda!</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootsrap.min.js"></script>
+    <script src="js/bundle.js"></script>
+</head>
+<body style="background: linear-gradient(90deg, white 65%, black 65%)">
+    <!-- Start Navbar Bercanda -->
+
+    <nav class="navbar navbar-expand-lg navbar-transparent bg-transparent">
+    <div class="container">
+      <a class="navbar-brand text-dark" href="#">
+      <img src="img/logo.png" width="100" height="40" class="d-inline-block align-top" alt=""></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link text-dark" href="#">Product <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-dark" href="#">How it Works</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Career</a>
+        </li>
+        </ul>
+    </div>
+            <a href="login.php" class="btn btn-light text-dark mx-2">Sign In</a>
+            <a href="register.php" class="btn btn-outline-light">Join Now!</a>
+    </div>
+    </nav>
+
+    <!-- End Navbar Bercanda -->
+
+    <!-- Start Content Bercanda -->
+    
+    <div class="container">
+        <div class="row m-4">
+            <div class="col-6 p-4">
+                <h1 class="display-2">Being a your <abbr title="Collab's your business Today!">Partner</abbr> and <br> Grow it Together</h1>
+            </div>
+            <div class="col-3">
+                <img src="img/teams.png" style="" alt="Image Intro" width="480" height="480">
+            </div>
+        </div>
+    </div>
+
+    <!-- End Content Bercanda -->
+
+    <!-- Start Script Bercanda -->
+
+    <script>alert("Under Development by KunCreative /w- @Farrid_jr");</script>
+
+    <!-- End Script Bercanda -->
+</body>
+</html>
