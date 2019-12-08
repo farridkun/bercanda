5c22b1b20a164bfa59daaf4054d6f2b6d74e1cbe
Farrid Kuntoro <farridkuntoro@email.unikom.ac.id>
2019-12-08 13:13:11 +0700

    disable function login and register

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,41 +10,42 @@
     <script src="js/bootsrap.min.js"></script>
     <script src="js/bundle.js"></script>
 </head>
+
 <body style="background: linear-gradient(90deg, white 65%, black 65%)">
     <!-- Start Navbar Bercanda -->
 
     <nav class="navbar navbar-expand-lg navbar-transparent bg-transparent">
-    <div class="container">
-      <a class="navbar-brand text-dark" href="#">
-      <img src="img/logo.png" width="100" height="40" class="d-inline-block align-top" alt=""></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-        <li class="nav-item active">
-            <a class="nav-link text-dark" href="#">Product <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-dark" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-dark" href="#">How it Works</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-dark" href="#">Career</a>
-        </li>
-        </ul>
-    </div>
-            <a href="login.php" class="btn btn-light text-dark mx-2">Sign In</a>
-            <a href="register.php" class="btn btn-outline-light">Join Now!</a>
-    </div>
+        <div class="container">
+            <a class="navbar-brand text-dark" href="#">
+                <img src="img/logo.png" width="100" height="40" class="d-inline-block align-top" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link text-dark" href="#">Product <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">How it Works</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Career</a>
+                    </li>
+                </ul>
+            </div>
+            <a href="#" class="btn btn-light text-dark mx-2">Sign In</a>
+            <a href="#" class="btn btn-outline-light">Join Now!</a>
+        </div>
     </nav>
 
     <!-- End Navbar Bercanda -->
 
     <!-- Start Content Bercanda -->
-    
+
     <div class="container">
         <div class="row m-4">
             <div class="col-6 p-4">
@@ -59,8 +61,11 @@
 
     <!-- Start Script Bercanda -->
 
-    <script>alert("Under Development by KunCreative /w- @Farrid_jr");</script>
+    <script>
+        alert("Under Development by KunCreative /w- @Farrid_jr");
+    </script>
 
     <!-- End Script Bercanda -->
 </body>
+
 </html>
